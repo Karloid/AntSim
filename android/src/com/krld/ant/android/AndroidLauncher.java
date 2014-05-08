6a5d3c545004c180b6208e6131e1b7691759ae88
karloid <krldboyan@gmail.com>
2014-05-08 07:39:48 +0400

    Initial commit

---
+++ android/src/com/krld/ant/android/AndroidLauncher.java
@@ -0,0 +1,16 @@
+package com.krld.ant.android;
+
+import android.os.Bundle;
+
+import com.badlogic.gdx.backends.android.AndroidApplication;
+import com.badlogic.gdx.backends.android.AndroidApplicationConfiguration;
+import com.krld.ant.AntView;
+
+public class AndroidLauncher extends AndroidApplication {
+	@Override
+	protected void onCreate (Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		AndroidApplicationConfiguration config = new AndroidApplicationConfiguration();
+		initialize(new AntView(), config);
+	}
+}
---
+++ desktop/src/com/krld/ant/desktop/DesktopLauncher.java
@@ -0,0 +1,15 @@
+package com.krld.ant.desktop;
+
+import com.badlogic.gdx.backends.lwjgl.LwjglApplication;
+import com.badlogic.gdx.backends.lwjgl.LwjglApplicationConfiguration;
+import com.krld.ant.AntView;
+
+public class DesktopLauncher {
+	public static void main (String[] arg) {
+		LwjglApplicationConfiguration config = new LwjglApplicationConfiguration();
+        config.width = 1280;
+        config.height = 780;
+
+		new LwjglApplication(new AntView(), config);
+	}
+}
